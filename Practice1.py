5cb9379ad8471690459fa0b38e96825411284047
rimi <singhrimiumesh@gmail.com>
2024-06-07 13:59:40 +0530

    DAY 1 Completed

--- Practice1.py
+++ Practice1.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+print("Hello World! \n")
+
+# this is single line comment
+# Write a program to print a poem in Python. Choose the poem of your choice and publish your repl
+print("This is a poem by me: \nWalk Fast \nTalk Less \nLife is hard \nLive it fast \nYou never Know when its last!")
+
+
+'''this is multiline comment
+code
+learn
+explore
+'''
+
+# escape seq. char.
+print("The sun is a \"star\".")
+
+
+# variables
+a = 1
+b =2.2
+c = complex(2, 4)
+d = True
+e = "Apple"
+f = None
+print("\nThe type of a is ", type(a))
+print("\nThe type of b is ", type(b))
+print("\nThe type of c is ", type(c))
+print("\nThe type of d is ", type(d))
+print("\nThe type of e is ", type(e))
+print("\nThe type of f is ", type(f))
---
+++ hardcode_calculator.py
@@ -0,0 +1,10 @@
+a = 20
+b = 3
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+print("Division(without decimal): ", a//b)
+print("Modulous: ", a%b)
+print("Exponential: ", a**3)
