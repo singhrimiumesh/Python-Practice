1f4cdaffb7c3804e16eceeb6eef7ab03b033fb31
rimi <singhrimiumesh@gmail.com>
2024-06-09 15:58:36 +0530

    Input

--- Practice2.py
+++ Practice2.py
@@ -4,3 +4,10 @@ b = 3.4
 
 print(a+b)
 # a+b is float ,as it is of higher order
+
+c = input("What\'s your name?\n")
+print("Hello!!! ", c)
+
+x =int(input())
+y = int(input())
+print(x+y)
---
+++ userinput_calcuator.py
@@ -0,0 +1,21 @@
+a = input("Enter 1st number: ")
+b = input("Enter 2nd number: ")
+
+# the inputs are string datatype
+# string calculations
+print(a+b, ": here a+b is a string concatination, as the inputs are of string datatype.\n So lets change the datatype of a and b to integer.")
+
+
+# Calcutions after converting into the integer
+x = int(a)
+y = int(b)
+
+print("Add: ",x+y)
+print("Sub: ", x-y)
+print("Muli: ",x*y)
+print("Divide: ",x/y)
+print("Divide(int): ",x//y)
+print("Modulous: ",x%y)
+
+
+
