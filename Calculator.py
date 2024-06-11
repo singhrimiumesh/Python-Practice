4be235dc142a11a5ce2aeeda424260ed32659c55
rimi <singhrimiumesh@gmail.com>
2024-06-11 15:21:27 +0530

    calculato

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Division: ", a/b)
+print("Multiplication: ", a*b)
+print("Modulous: ", a%b)
