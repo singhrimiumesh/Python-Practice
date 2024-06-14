d404318e2323ae2c47428cebb0404274b059c527
rimi <singhrimiumesh@gmail.com>
2024-06-14 21:29:34 +0530

    if

--- practice.py
+++ practice.py
@@ -8,3 +8,9 @@ if x == 30 or x <= 30:
     print("Valid")
 else:
     print("Invalid")
+
+is_sunny = "True"
+if is_sunny == "True":
+    print("It's Sunny today.")
+else:
+    print("It's rainy today.")
