21458e9f8a62b18cf49655ee801ba1284b9bae5c
rimi <singhrimiumesh@gmail.com>
2024-06-18 19:30:07 +0530

    greet

--- Greeting.py
+++ Greeting.py
@@ -1,14 +1,15 @@
 import time
 
 current_time = time.strftime('%H:%M:%S')
-print("Time-",current_time)
+print("Time: ",current_time)
+
 current_hour = int(time.strftime('%H'))
 
 if current_hour >= 5 and current_hour < 12:
-    print("Good Morning")
+    print("Good Morning!")
 elif current_hour >= 12 and current_hour < 17:
-    print("Good Afternoon")
+    print("Good Afternoon!")
 elif current_hour >= 17 and current_hour < 21:
-    print("Good Evening")
+    print("Good Evening!")
 else:
-    print("Good Night")
+    print("Good Night!")
